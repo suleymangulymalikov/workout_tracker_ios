4d936f2b5df617dee12a18671ffa0fbb566acb12
J M <272664@student.pwr.edu.pl>
2025-11-03 18:51:59 +0100

    feat: Finished the Home Screen

---
+++ Workout Tracker/HomeActionCard.swift
@@ -0,0 +1,39 @@
+//
+//  HomeActionCard.swift
+//  Workout Tracker
+//
+//  Created by stud on 03/11/2025.
+//
+import SwiftUI
+
+struct HomeActionCard: View {
+    let title: String
+    let color: Color
+    let icon: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 20)
+            .fill(color.opacity(0.15))
+            .frame(height: 200)
+            .overlay(
+                
+                
+                VStack(spacing: 10){
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(color)
+                        .frame(height: 60)
+                        .frame(width: 60)
+                        .overlay(
+                            Image(systemName: icon)
+                                .font(.system(size: 30))
+                                .foregroundStyle(.white)
+                            
+                        )
+                    
+                    Text(title)
+                        .font(.headline)
+                        .foregroundColor(.black)
+                }
+            )
+    }
+}
