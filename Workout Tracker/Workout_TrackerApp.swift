ea35b58cee92a2736af62f86db7ce4f1f59c8d7b
J M <272664@student.pwr.edu.pl>
2025-11-03 17:13:36 +0100

    little changes

--- Workout Tracker/Workout_TrackerApp.swift
+++ Workout Tracker/Workout_TrackerApp.swift
@@ -12,7 +12,7 @@ import SwiftData
 struct Workout_TrackerApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Item.self,
+            
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
